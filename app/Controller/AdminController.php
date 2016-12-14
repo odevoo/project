8c0e3afed92ae696a625a5165004e87130b7f321
odevoo <elandry@odevoo.com>
2016-12-14 14:35:06 +0100

    commit

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -16,10 +16,13 @@ class AdminController extends Controller
         $this->show('admin/register');
     }
     public function processRegisterForm() {
-        //debug($_POST);
-        $student = new StudentModel ($_POST['firstname'], $_POST['lastname'], $_POST['password'], $_POST['email'], $_POST['address']);
-        debug($student);
-        $student->insert(['firstname' => $this->getFirstname(), 'lastname' => $this->getLastname()]);
+        debug($_POST);
+        if ($_POST['type'] === 'student') {
+            $student = new StudentModel ($_POST['firstname'], $_POST['lastname'], $_POST['password'], $_POST['email'], $_POST['address']);
+            //debug($student->getFirstname());
+            $student->insert(['firstname' => $student->getFirstname(), 'lastname' => $student->getLastname()]);
+        }
+        
 
     }
 
