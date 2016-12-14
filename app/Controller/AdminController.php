b8cc6c840e698ae50c200a3010931a287aae23c1
tenimas <ophois34@gamil.com>
2016-12-14 14:36:30 +0100

    Création de la class Teacher

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -4,6 +4,7 @@ namespace Controller;
 
 use \W\Controller\Controller;
 use \Model\StudentModel;
+use \Model\TeacherModel;
 
 class AdminController extends Controller
 {
@@ -17,9 +18,14 @@ class AdminController extends Controller
     }
     public function processRegisterForm() {
         //debug($_POST);
-        $student = new StudentModel ($_POST['firstname'], $_POST['lastname'], $_POST['password'], $_POST['email'], $_POST['address']);
-        debug($student);
-        $student->insert(['firstname' => $this->getFirstname(), 'lastname' => $this->getLastname()]);
+        // $student = new StudentModel ($_POST['firstname'], $_POST['lastname'], $_POST['password'], $_POST['email'], $_POST['address']);
+        // debug($student);
+        // $student->insert(['firstname' => $this->getFirstname(), 'lastname' => $this->getLastname()]);
+
+
+        $teacher = new TeacherModel ($_POST['firstname'], $_POST['lastname'], $_POST['password'], $_POST['email'], $_POST['address']);
+        debug($teacher);
+        $teacher->insert(['firstname' => $teacher->getFirstname(), 'lastname' => $teacher->getLastname()]);
 
     }
 
