ec07da7688029c5ffaf845001a1bddf67aae7814
CAETANO Gérald <caetano.gerald@gmail.com>
2016-12-15 17:00:59 +0100

    Layout & Home

--- app/routes.php
+++ app/routes.php
@@ -1,12 +1,15 @@
 <?php
 
-	$w_routes = array(
-		['GET', '/', 'Default#home', 'default_home'],
+	$w_routes = array(        
+		/* Acceuil */
+		['GET', '/home', 'Default#home', 'default_home'],
 
         /*Connexion / Inscription */
 
         ['GET', '/register', 'Admin#showRegisterForm', 'admin_register'],
         ['POST', '/register', 'Admin#processRegisterForm', 'admin_process_register'],
+
+
 
 
 	);
