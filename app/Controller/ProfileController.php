c33d08d76e049b98237d7f6572517eed22af569d
tenimas <ophois34@gamil.com>
2016-12-16 16:51:14 +0100

    Profile

--- app/Controller/ProfileController.php
+++ app/Controller/ProfileController.php
@@ -4,19 +4,24 @@ namespace Controller;
 
 use \Model\SubjectModel;
 use \Model\TeacherModel;
+use \Model\LevelModel;
 
 class ProfileController extends \W\Controller\Controller
 {
-  
+
 
     //Affiche la page profile
     public function showProfile($id)
     {
+        $subjects = new SubjectModel;
+        $subjectsData = $subjects->findTeacherSubjects($id);
+        $level = new LevelModel;
+        $levelData = $level->findTeacherLevel($id);
         $teacher = new TeacherModel;
         $teacherdata = $teacher->find($id);
-        $this->show('profile/profile',['teacher' => $teacherdata]);
+        $this->show('profile/profile',['teacher' => $teacherdata, 'subjects' => $subjectsData, 'level' => $levelData]);
     }
-    
 
-    
+
+
 }
--- app/Model/SubjectModel.php
+++ app/Model/SubjectModel.php
@@ -3,10 +3,10 @@ namespace Model;
 
 use \W\Model\ConnectionModel;
 
-class SubjectModel extends \W\Model\Model 
+class SubjectModel extends \W\Model\Model
 {
     private $name;
-    
+
 
     public function __construct($name ='') {
         $app = getApp();
@@ -25,5 +25,19 @@ class SubjectModel extends \W\Model\Model
     public function getName() {
         return $this->name;
     }
- 
+
+    public function findTeacherSubjects($id) {
+      $sql = 'SELECT s.id , name
+              FROM subjects s
+              INNER JOIN expertises e
+              ON s.id = e.id_subject
+              INNER JOIN users u
+              ON e.id_teacher = u.id
+              WHERE
+              u.id = :id';
+      $stmt = $this->dbh->prepare($sql);
+      $stmt->execute(array(':id' => $id));
+      $teachers = $stmt->fetchAll();
+      return $teachers;
+    }
 }
