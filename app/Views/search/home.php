585343fc7377902836d9d2553af1e49853234e17
JP COLLIAUX <jpcolliaux@gmail.com>
2016-12-16 16:51:11 +0100

    vendredi soir JP

--- app/Views/search/home.php
+++ app/Views/search/home.php
@@ -5,13 +5,38 @@ $this->layout('layout', ['title' => 'Page de recherche'])
 <?php
 //début du bloc main_content
 $this->start('main_content'); ?>
+
+
+<div class="container">
+ <?php foreach (array_chunk($subjects, 6 , true) as $subjects) : ?>
+	<div class="row">
+
 <?php
-foreach($subjects as $subject){
+foreach($subjects as $subject) : ?>
+<?php
     $id = $subject['id'];
     $name = $subject['name'];
-    echo $id.'=>'.$name.'<br/>';
-}
-?>
+    $img = $subject['img'];
+//    echo $id.'=>'.$name.'=>'.$img.'<br/>';
+ ?>
+
+		<div class="col-md-2">
+		<a href="<?= $this->url('search_result',["id" => $id]); ?>">
+		<img src="<?php echo $this->assetUrl($img); ?>" class="img-responsive img-rounded" alt="logo" />
+		<h4 class="text-center"><?php echo $name ?></h4>
+		</a>
+	</div>
+	 <?php endforeach; ?>
+	</div>
+		 <?php endforeach; ?>
+</div>
+
+
+
+
+
+
+
 <input type="hidden" name="" id="id-student" value="<?= $student['id'] ?>">
 <input type="hidden" id="img-maps" name="" value="<?= $this->assetUrl('img/pencil-case.png'); ?>">
 <div style="height:900px;width:100%;" id="js-map-container" class="map"></div>
