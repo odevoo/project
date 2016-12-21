cfcacf8f66339bf34f219adf783800373a12aa4e
odevoo <elandry@odevoo.com>
2016-12-21 19:35:08 +0100

    commit

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -7,6 +7,7 @@ use \W\Security\AuthentificationModel;
 use \Model\StudentModel;
 use \Model\TeacherModel;
 use \Model\LevelModel;
+use \Model\LessonsModel;
 use \Model\ExpertiseModel;
 use \Model\SubjectModel;
 use \Controller\SearchController;
@@ -222,15 +223,157 @@ class AdminController extends Controller
     public function generatePdf() {
       debug($_POST);
       //$pdf = new Pdf('../../docs/index.html');
-      $newcontent = file_get_contents('C:\xampp\htdocs\project\docs\test.html');
+      //$newcontent = file_get_contents('C:\xampp\htdocs\project\docs\test.html');
+      $lesson = new LessonsModel;
+      $lessons = $lesson->getLessonsByStatutTeacher($_SESSION['user']['id'], 4);
+      $i = 1;
+      $array = [];
+      foreach ($lessons as $lesson) {
+        $array[$i] = $lesson['firstname'].'&nbsp'.$lesson['lastname'].'&nbsp'.date("d-m-Y", strtotime($lesson['date'])).'&nbsp'.$lesson['hstart'].':00';
+        $i++;
+      }
+
+
+      $newcontent = '<div id="invoiceholder">
+
+  <div id="headerimage"></div>
+  <div id="invoice" class="effect2">
+    
+    <div id="invoice-top">
+      <div class="logo"></div>
+      <div class="info">
+        <h2>Michael Truong</h2>
+        <p> hello@michaeltruong.ca </br>
+            289-335-6503
+        </p>
+      </div><!--End Info-->
+      <div class="title">
+        <h1>Invoice #1069</h1>
+        <p>Issued: May 27, 2015</br>
+           Payment Due: June 27, 2015
+        </p>
+      </div><!--End Title-->
+    </div><!--End InvoiceTop-->
+
+
+    
+    <div id="invoice-mid">
+      
+      <div class="clientlogo"></div>
+      <div class="info">
+        <h2>Client Name</h2>
+        <p>JohnDoe@gmail.com</br>
+           555-555-5555</br>
+      </div>
+
+      <div id="project">
+        <h2>Project Description</h2>
+        <p>Proin cursus, dui non tincidunt elementum, tortor ex feugiat enim, at elementum enim quam vel purus. Curabitur semper malesuada urna ut suscipit.</p>
+      </div>   
+
+    </div><!--End Invoice Mid-->
+    
+    <div id="invoice-bot">
+      
+      <div id="table">
+        <table>
+          <tr class="tabletitle">
+            <td class="item"><h2>Item Description</h2></td>
+            <td class="Hours"><h2>Hours</h2></td>
+            <td class="Rate"><h2>Rate</h2></td>
+            <td class="subtotal"><h2>Sub-total</h2></td>
+          </tr>
+          
+          <tr class="service">
+            <td class="tableitem"><p class="itemtext">Communication</p></td>
+            <td class="tableitem"><p class="itemtext">5</p></td>
+            <td class="tableitem"><p class="itemtext">$75</p></td>
+            <td class="tableitem"><p class="itemtext">$375.00</p></td>
+          </tr>
+          
+          <tr class="service">
+            <td class="tableitem"><p class="itemtext">Asset Gathering</p></td>
+            <td class="tableitem"><p class="itemtext">3</p></td>
+            <td class="tableitem"><p class="itemtext">$75</p></td>
+            <td class="tableitem"><p class="itemtext">$225.00</p></td>
+          </tr>
+          
+          <tr class="service">
+            <td class="tableitem"><p class="itemtext">Design Development</p></td>
+            <td class="tableitem"><p class="itemtext">5</p></td>
+            <td class="tableitem"><p class="itemtext">$75</p></td>
+            <td class="tableitem"><p class="itemtext">$375.00</p></td>
+          </tr>
+          
+          <tr class="service">
+            <td class="tableitem"><p class="itemtext">Animation</p></td>
+            <td class="tableitem"><p class="itemtext">20</p></td>
+            <td class="tableitem"><p class="itemtext">$75</p></td>
+            <td class="tableitem"><p class="itemtext">$1,500.00</p></td>
+          </tr>
+          
+          <tr class="service">
+            <td class="tableitem"><p class="itemtext">Animation Revisions</p></td>
+            <td class="tableitem"><p class="itemtext">10</p></td>
+            <td class="tableitem"><p class="itemtext">$75</p></td>
+            <td class="tableitem"><p class="itemtext">$750.00</p></td>
+          </tr>
+          
+          <tr class="service">
+            <td class="tableitem"><p class="itemtext"></p></td>
+            <td class="tableitem"><p class="itemtext">HST</p></td>
+            <td class="tableitem"><p class="itemtext">13%</p></td>
+            <td class="tableitem"><p class="itemtext">$419.25</p></td>
+          </tr>
+          
+            
+          <tr class="tabletitle">
+            <td></td>
+            <td></td>
+            <td class="Rate"><h2>Total</h2></td>
+            <td class="payment"><h2>$3,644.25</h2></td>
+          </tr>
+          
+        </table>
+      </div><!--End Table-->
+      
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+      <input type="hidden" name="cmd" value="_s-xclick">
+      <input type="hidden" name="hosted_button_id" value="QRZ7QTM9XRPJ6">
+      <input type="image" src="http://michaeltruong.ca/images/paypal.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+    </form>
+
+      
+      <div id="legalcopy">
+        <p class="legal"><strong>Thank you for your business!</strong>  Payment is expected within 31 days; please process this invoice within that time. There will be a 5% interest charge per month on late invoices. 
+        </p>
+      </div>
+      
+    </div><!--End InvoiceBot-->
+  </div><!--End Invoice-->
+</div><!-- End Invoice Holder-->';
 
       if (!file_exists('C:\xampp\htdocs\project\docs\newfile.html')) { 
         $handle = fopen('C:\xampp\htdocs\project\docs\newfile.html','w+'); 
         fwrite($handle,$newcontent); 
         fclose($handle); }
 
+
+        exec('C:\wkhtmltopdf\bin\wkhtmltopdf.exe C:\xampp\htdocs\project\docs\newfile.html form1.pdf');
+       
+        header('Pragma: public');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Content-Type: application/pdf');
+        header('Content-Transfer-Encoding: binary');
+        header("Content-Disposition: attachment; filename=report.pdf");
+        //header('Content-Length: '.filesize($this->_fileName));
+       
+        readfile('C:\xampp\htdocs\project\public\form1.pdf');
+        /*
       $pdf = new Pdf(array(
-        'no-outline',         // Make Chrome not complain
+        'no-outline',  
+        'encoding' => 'UTF-8',       
         'margin-top'    => 0,
         'margin-right'  => 0,
         'margin-bottom' => 0,
@@ -239,13 +382,14 @@ class AdminController extends Controller
         // Default page options
         'disable-smart-shrinking',
         //'user-style-sheet' => 'C:\xampp\htdocs\project\docs\test.css',
+        'user-style-sheet' => 'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css',
       ));
       $pdf->binary = 'C:\wkhtmltopdf\bin\wkhtmltopdf.exe';
       $pdf->addPage('C:\xampp\htdocs\project\docs\newfile.html');
-      //$pdf->addPage('https://www.google.fr');
+      //$pdf->addPage('http://google.com');
       if (!$pdf->send('report.pdf')) {
         echo $pdf->getError();
-        }
+        }*/
     }
 
     public function uploadRib() {
